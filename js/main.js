b40ed6800127f4c9ec28307357923b31a7db638e
Jonathan Bourke <jonathan@bourke.net.nz>
2014-03-06 13:04:19 +1100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 
-
 (function ($) {
-    "use strict";
-
-    $(document).ready(function() {
+	"use strict";
+	
+	$(document).ready(function() {
 		$(window).scroll(function() {
-			
+		
 			var b = $("body"), t = b.scrollTop(), h = $("header#site-header");
 			(t > 0) ? h.addClass("is-sticky") : h.removeClass("is-sticky");
-			
+		
 		});
 	});
 	
+	hljs.configure({
+		languages: ['cs']
+	});
+	
+	hljs.initHighlightingOnLoad();
 }(jQuery));
