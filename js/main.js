a8c800bea794085505f56272646ebb31fe453759
Jonathan <jonathan@bourke.net.nz>
2014-03-08 15:47:47 +1100

    Changing layout

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
 	$(document).ready(function() {
 		$(window).scroll(function() {
 		
-			var b = $("body"), t = b.scrollTop(), h = $("header#site-header");
-			(t > 0) ? h.addClass("is-sticky") : h.removeClass("is-sticky");
-		
 		});
 	});
 }(jQuery));
